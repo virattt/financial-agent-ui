4116cb178be2b4d15a0e8e0fa6f68c4124ef7239
virattt <virat.dot@gmail.com>
2024-04-08 17:03:44 -0400

    Format chat.tsx

--- src/components/llm/chat.tsx
+++ src/components/llm/chat.tsx
@@ -1,43 +1,39 @@
 "use client";
 
 import React, { useState } from "react";
-import { useUIState, useActions, useAIState } from "ai/rsc";
-import { AI } from "@/app/action";
-import { useScrollAnchor } from "@/hooks/use-scroll-anchor";
+import { useAIState, useUIState } from "ai/rsc";
 import { MessageList } from "@/components/llm/message-list";
 import { cn } from "@/lib/utils";
 import { ChatInput } from "@/components/llm/chat-input";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-} from "@/components/ui/card";
-const examples = [
-  "Compare the stock price of AAPL, NVDA and MSFT for the last 30 days, the news, and give a detailed analysis and what looks the most bullish",
-  "What are the key factors driving the recent stock price movements of Tesla (TSLA)? Provide a brief analysis.",
-];
+import { Card, CardContent, CardHeader, } from "@/components/ui/card";
+
 export function Chat() {
   const [messages] = useUIState();
   const [input, setInput] = useState("");
   const [aiState] = useAIState();
 
   return (
-    <div className="group w-full overflow-auto pl-0 peer-[[data-state=open]]:lg:pl-[250px] peer-[[data-state=open]]:xl:pl-[300px]">
+    <div
+      className="group w-full overflow-auto pl-0 peer-[[data-state=open]]:lg:pl-[250px] peer-[[data-state=open]]:xl:pl-[300px]">
       {messages.length === 0 ? (
-        <IntroSection setInput={setInput} />
+        <IntroSection setInput={setInput}/>
       ) : (
         <div className={cn("pb-[200px] pt-4 md:pt-10")}>
-          <MessageList messages={messages} />
+          <MessageList messages={messages}/>
         </div>
       )}
 
-      <ChatInput input={input} setInput={setInput} />
+      <ChatInput input={input} setInput={setInput}/>
     </div>
   );
 }
 
 function IntroSection({ setInput }: { setInput: (input: string) => void }) {
+  const examples = [
+    "Compare the stock price of AAPL, NVDA and MSFT for the last 30 days, the news, and give a detailed analysis and what looks the most bullish",
+    "What are the key factors driving the recent stock price movements of Tesla (TSLA)? Provide a brief analysis.",
+  ];
+
   return (
     <div className="flex justify-center items-center h-full">
       <Card className="sm:mx-0 max-w-screen-md rounded-md border sm:w-full">
