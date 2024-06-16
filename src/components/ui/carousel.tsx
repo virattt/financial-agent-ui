81874339acec6350bbf30a6c5353603b31c6e294
virattt <virat.dot@gmail.com>
2024-06-16 15:00:28 -0400

    Clean up

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -216,7 +216,7 @@ const CarouselPrevious = React.forwardRef<
       onClick={scrollPrev}
       {...props}
     >
-      <ArrowLeft className="h-4 w-4" />
+      <ArrowLeft className="h-4 w-4"/>
       <span className="sr-only">Previous slide</span>
     </Button>
   )
@@ -245,7 +245,7 @@ const CarouselNext = React.forwardRef<
       onClick={scrollNext}
       {...props}
     >
-      <ArrowRight className="h-4 w-4" />
+      <ArrowRight className="h-4 w-4"/>
       <span className="sr-only">Next slide</span>
     </Button>
   )
