627d1c4623e8600ee788c2f2dde4f96fe1c2222c
virattt <virat.dot@gmail.com>
2024-09-13 09:40:19 -0400

    Add hot reloading

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   env: {
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  },
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
   },
   // You can add other Next.js configuration options here
 };
