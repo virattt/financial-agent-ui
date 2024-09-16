403b6810a3c7c2241b2399c00a803582f9d0258a
virattt <virat.dot@gmail.com>
2024-09-16 18:20:22 -0400

    Change AIMessage bg color

--- frontend/components/prebuilt/message.tsx
+++ frontend/components/prebuilt/message.tsx
@@ -6,7 +6,7 @@ export interface MessageTextProps {
 
 export function AIMessageText(props: MessageTextProps) {
   return (
-    <div className="flex mr-auto w-fit max-w-[700px] bg-blue-400 rounded-md px-2 py-1 mt-3">
+    <div className="flex mr-auto w-fit max-w-[700px] bg-[#0A0C10] rounded-md px-2 py-1 mt-3">
       <p className="text-normal text-gray-50 text-left break-words">
         <Markdown>{props.content}</Markdown>
       </p>
